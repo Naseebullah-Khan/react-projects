dc1633d29e4f1a0f8cbb2e212c49e4763666843f
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-12-22 00:03:25 +0430

    Fix SingleProductPage to pass image prop correctly to ProductImages component

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -57,7 +57,7 @@ const SingleProductPage = () => {
           back to products
         </Link>
         <div className="product-center">
-          <ProductImages {...image} />
+          <ProductImages image={image} />
           <div className="content">
             <h2>{name}</h2>
             <Stars data={{ stars, reviews }} />
